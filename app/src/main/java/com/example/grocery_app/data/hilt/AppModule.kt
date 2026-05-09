39f0ed29a5308aec4edf9c8e819b7acf834eea48
Hawkjeet <jeetmulani2006@gmail.com>
2026-05-09 15:52:45 +0530

    Hilt Setup

---
+++ app/src/main/java/com/example/grocery_app/data/hilt/AppModule.kt
@@ -0,0 +1,42 @@
+package com.example.grocery_app.data.hilt
+
+
+import android.content.Context
+import com.example.grocery_app.data.remote.CartRepository
+import com.example.grocery_app.data.remote.ProductRepository
+import com.example.grocery_app.data.room.AppDatabase
+import com.example.grocery_app.data.room.CartDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideAppDatabase(@ApplicationContext context: Context): AppDatabase {
+        return AppDatabase.getDatabase(context)
+    }
+
+    @Provides
+    fun provideCartDao(appDatabase: AppDatabase): CartDao {
+        return appDatabase.cartDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideCartRepository(cartDao: CartDao): CartRepository {
+        return CartRepository(cartDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideProductRepository(): ProductRepository {
+        return ProductRepository()
+    }
+}
